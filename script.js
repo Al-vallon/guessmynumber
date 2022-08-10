961ca182296edfd1c53129c67983c5a7b2025629
alex <81435330+Al-vallon@users.noreply.github.com>
2022-08-10 21:04:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,60 +2,66 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     
     let secretNumber = Math.trunc(Math.random()*20) +1;
-    let scoredec = 20;
-    let highscoredec = 20;
+    let scoredec = 10;
+    let highscoredec = 10;
+    console.log(secretNumber);
 
-    const message = document.querySelector('.message');
     const win = document.querySelector('.win');
+    const loose = document.querySelector ('.loose')
     const check = document.querySelector('.check');
     const reset = document.querySelector('.reset');
     const score = document.querySelector('.score');
     const highscore = document.querySelector('.highscore');
 
-    console.log(secretNumber);    
+    const message = function (message){
+        document.querySelector('.message').textContent = message;
+    };
+    
+    
+    const audio  = new Audio('https://www.televisiontunes.com/uploads/audio/Breaking%20Bad.mp3');
+    const laugth = new Audio('https://www.fesliyanstudios.com/play-mp3/3976');
 
-    check.addEventListener('click', function (){
+    check.addEventListener ('click', function (){
+
         const guess = Number(document.querySelector('.guess').value);
 
+        //no input
         if(!guess){
-            message.textContent = '❌ Sorry, can you entrer a valid number, please';
+            message('❌ Sorry, can you entrer a valid number, please');
+        
+        // when player wins
         } else if ( guess == secretNumber){
-            message.textContent = '🎉 Yoo hoo, great Job, you find the secret number !! 🎉';
+            message('🎉 Yoo hoo, great Job, you find the secret number !! 🎉');
             win.style.display = 'inline';
-        }
-        
-        if (guess > secretNumber){
-            message.textContent = '⏫ too high !!';
-            scoredec--
-            score.textContent = scoredec;
-            highscoredec--;
-            highscore.textContent = highscoredec
+            audio.play();
 
-        } else if (guess < secretNumber){
-            message.textContent = '⏬ too low !!';
-            // scoredec--
-            // score.textContent = scoredec;
-            // highscoredec--;
-            // highscore.textContent = highscoredec
-            
-        }
+            if(scoredec > highscoredec){
+                highscoredec = scoredec
+                highscore.textContent = highscoredec;
+            }
         
-    })
+        } else if (guess !== secretNumber) {
+            if(scoredec > 1){
+                message(guess > secretNumber ? '⏫ too high !!': '⏬ too low !!');
+                scoredec--;
+                score.textContent = scoredec;
+            } else {
+                message('😿 You loose the game 😿');
+                loose.style.display = 'inline';
+                score.textContent = 0;
+                laugth.play();
+                }      
+        }         
+    });
 
 
     reset.addEventListener('click', function(){
         secretNumber = Math.trunc(Math.random()*20) +1;
         win.style.display = 'none';
-        message.textContent = 'Starting guess . . .';
-        score.textContent = 20;
+        loose.style.display = 'none';
+        message('Starting guess . . .');
+        score.textContent = 10;
         document.querySelector('.guess').value = '';
     });
-
-    
-    
-
-
-
-
     console.log("Loaded");
 });

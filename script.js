9add6147c5840814b51c362ff40a797bbd856c7c
alex <81435330+Al-vallon@users.noreply.github.com>
2022-08-12 13:56:28 +0200

    add player name localsession

--- script.js
+++ script.js
@@ -7,22 +7,31 @@ window.addEventListener("DOMContentLoaded", (event) => {
     console.log(secretNumber);
 
     const win = document.querySelector('.win');
-    const loose = document.querySelector ('.loose')
+    const loose = document.querySelector ('.loose');
     const check = document.querySelector('.check');
     const reset = document.querySelector('.reset');
     const score = document.querySelector('.score');
     const highscore = document.querySelector('.highscore');
+    const enter = document.querySelector('.validPseudo');
+    const audio  = new Audio('https://www.televisiontunes.com/uploads/audio/Breaking%20Bad.mp3');
+    const laugth = new Audio('https://www.fesliyanstudios.com/play-mp3/3976');
+    const h2 = document.querySelector('.hello');
+    // let inputPseudo = document.querySelector('.pseudo')
+
 
     const message = function (message){
         document.querySelector('.message').textContent = message;
     };
     
+    pseudo
+    enter.addEventListener('click', function(){
+        localStorage.setItem('pseudo', pseudo.value);
+            if(localStorage.getItem('pseudo')!== null){
+                h2.textContent = `Welcome ${localStorage.getItem('pseudo')} 🤘`
+            }
+    });
     
-    const audio  = new Audio('https://www.televisiontunes.com/uploads/audio/Breaking%20Bad.mp3');
-    const laugth = new Audio('https://www.fesliyanstudios.com/play-mp3/3976');
-
     check.addEventListener ('click', function (){
-
         const guess = Number(document.querySelector('.guess').value);
 
         //no input
@@ -45,6 +54,8 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 message(guess > secretNumber ? '⏫ too high !!': '⏬ too low !!');
                 scoredec--;
                 score.textContent = scoredec;
+                highscoredec--
+                highscore.textContent = highscoredec
             } else {
                 message('😿 You loose the game 😿');
                 loose.style.display = 'inline';
@@ -61,6 +72,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
         loose.style.display = 'none';
         message('Starting guess . . .');
         score.textContent = 10;
+        scoredec = 10
         document.querySelector('.guess').value = '';
     });
     console.log("Loaded");
